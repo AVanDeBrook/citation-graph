c808f5d6083a763b1215db30362984e0cf96f99b
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-02-23 14:09:14 -0500

    make changes to doxygen config file, generate HTML (not on anything useful, just to see)

---
+++ data/out/c/example.c
@@ -0,0 +1,26 @@
+/*!
+\param Abstract While the channel estimation results from these algorithmscan be impressivrina Del Greco/AppData/Local/Programs/Python/Python38-32/python.exe" "c:/Users/Corrina Del Greco/Documents/ERAU/Senior Design/citation-e for the FIR channels, the algorithms can hardly beapplied in real-world applications. Th                                                                                                                                       graph/src/main.py"is paper explains in detailthe reason why the FIR channel model is only an approximate onerina Del Greco/AppData/Local/Programs/Python/Python38-32/python.exe" "c:/Users/Corrina Del Greco/Documents/ERAU/Senior Design/citation- forthe OFDM-based communication systems, trying to discourage the inappropriate usageof this model, which can lead to fruitless efforts. This paper alsopresents a realistic channeigh data rate transmissions over time-dispersive fadingchannels. Many OFDM-based communication systems use packet-basedtransmission, whl model for the OFDM-based communicationsystems, which can be used to access the channel p finite impulse response(FIR) channel model is simple and effective for some simulations ofthe OFDM-based communication systems over tharameterestimation algorithmsrealistically.
+\param Title The Title of a Paper
+\param Author Abramson
+*/
+void Abramson1963(Abstract abstract, Title title, Author author) {
+    Abidi1995(x, x, x);
+};
+
+/*!
+\param Abstract While the channel estimation results from these algorithmscan be impressivrina Del Greco/AppData/Local/Programs/Python/Python38-32/python.exe" "c:/Users/Corrina Del Greco/Documents/ERAU/Senior Design/citation-e for the FIR channels, the algorithms can hardly beapplied in real-world applications. Th                                                                                                                                       graph/src/main.py"is paper explains in detailthe reason why the FIR channel model is only an approximate onerina Del Greco/AppData/Local/Programs/Python/Python38-32/python.exe" "c:/Users/Corrina Del Greco/Documents/ERAU/Senior Design/citation- forthe OFDM-based communication systems, trying to discourage the inappropriate usageof this model, which can lead to fruitless efforts. This paper alsopresents a realistic channeigh data rate transmissions over time-dispersive fadingchannels. Many OFDM-based communication systems use packet-basedtransmission, whl model for the OFDM-based communicationsystems, which can be used to access the channel p finite impulse response(FIR) channel model is simple and effective for some simulations ofthe OFDM-based communication systems over tharameterestimation algorithmsrealistically.
+\param Title The Title of Another Paper
+\param Author Abidi
+*/
+void Abidi1995(Abstract abstract, Title title, Author author) {
+    Liu2008(x, x, x);
+};
+
+/*!
+\param Abstract While the channel estimation results from these algorithmscan be impressivrina Del Greco/AppData/Local/Programs/Python/Python38-32/python.exe" "c:/Users/Corrina Del Greco/Documents/ERAU/Senior Design/citation-e for the FIR channels, the algorithms can hardly beapplied in real-world applications. Th                                                                                                                                       graph/src/main.py"is paper explains in detailthe reason why the FIR channel model is only an approximate onerina Del Greco/AppData/Local/Programs/Python/Python38-32/python.exe" "c:/Users/Corrina Del Greco/Documents/ERAU/Senior Design/citation- forthe OFDM-based communication systems, trying to discourage the inappropriate usageof this model, which can lead to fruitless efforts. This paper alsopresents a realistic channeigh data rate transmissions over time-dispersive fadingchannels. Many OFDM-based communication systems use packet-basedtransmission, whl model for the OFDM-based communicationsystems, which can be used to access the channel p finite impulse response(FIR) channel model is simple and effective for some simulations ofthe OFDM-based communication systems over tharameterestimation algorithmsrealistically.
+\param Title The Title of the Last Paper
+\param Author Liu
+*/
+void Liu2008(Abstract abstract, Title title, Author author) {
+
+};
--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 import os
-import subprocess
+import webbrowser
 
 # generate the doxygen configuration file
 os.system("doxygen -g data/out/doxygen/config/Doxyfile")
 
 # generate HTML documentation from config file
 os.system("doxygen data/out/doxygen/config/Doxyfile")
+
+# open HTML in browser
+webbrowser.open("data\out\doxygen\html\index.html")
