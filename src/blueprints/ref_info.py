931930d5836e620da68b289da5f1872e4a9c15ed
Aaron Van De Brook <avandebrook5@gmail.com>
2021-03-22 21:17:30 -0400

    Add editable form template, server now handles and saves papers.

--- src/blueprints/ref_info.py
+++ src/blueprints/ref_info.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-
+import os
+# from cwriter import CWriter
+from parsers.bibtexparser import BibtexParser
+from parsers.latexparser import LatexParser
 from flask import *
+from multiprocessing import Process
 
 bp = Blueprint('papers', __name__, url_prefix='/papers')
 
@@ -11,4 +15,39 @@ def papers_home():
 @bp.route('/new', methods=('GET', 'POST'))
 def new_paper():
 	if request.method == 'POST':
-		return request.form['texFile']
+		tex_file = request.files['texFile']
+		bib_file = request.files['bibFile']
+		file_path = os.path.join(current_app.config['USER_PAPERS'], tex_file.filename.split('.')[0])
+		bibtex_refs = []
+
+		try:
+			os.makedirs(file_path)
+		except OSError:
+			# if folder already exists we can use the database to fill out the form data
+			pass
+
+		tex_file.save(os.path.join(file_path, tex_file.filename))
+		bib_file.save(os.path.join(file_path, bib_file.filename))
+
+		latex_parser = LatexParser(os.path.join(file_path, tex_file.filename))
+		bibtex_parser = BibtexParser(
+			bib_file=os.path.join(file_path, bib_file.filename),
+			keys_list={
+				"ID",
+				"title",
+				"author",
+				"year"
+			}
+		)
+
+		for entry in bibtex_parser.dict_entries:
+			if entry['ID'] in latex_parser.get_citation_list():
+				bibtex_refs.append(entry)
+
+		# CWriter(
+		# 	os.path.join(file_path, bib_file.filename),
+		# 	os.path.join(file_path, tex_file.filename),
+		# 	os.path.join(file_path, tex_file.filename + ".c")
+		# )
+
+		return render_template('reference_info.html', refs=bibtex_refs, lp=latex_parser)
--- src/server.py
+++ src/server.py
@@ -17,12 +17,19 @@ def create_app(test_config=None):
 		instance_relative_config=True,
 	)
 
+	app.config.from_mapping(
+		USER_PAPERS=os.path.join(app.instance_path,'user_files', 'papers')
+	)
 
 	if test_config is None:
 		app.config.from_pyfile('config.py', silent=True)
 	else:
 		app.config.from_mapping(test_config)
 
+	try:
+		os.makedirs(os.path.join(app.instance_path, 'user_files', 'papers'))
+	except OSError as e:
+		pass
 
 	@app.route('/')
 	def index():
