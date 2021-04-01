37150ef2c9dafe165c3b5d04e5ecf7dc0dd6ea34
Corrina Del Greco <corrinadelgreco@gmail.com>
2021-04-01 12:13:42 -0400

    init db when creating app

--- src/server.py
+++ src/server.py
@@ -11,6 +11,15 @@ from blueprints import ref_info, lookup_paper
 # open HTML in browser
 # webbrowser.open("../data/out/doxygen/html/index.html")
 
+DATABASE = 'citation_graph.db'
+QUERY_GET_PAPER_BY_ID = 'SELECT * FROM paper WHERE paper_id = ?'
+QUERY_GET_PAPERS_BY_ATTRIBUTE = 'SELECT * FROM paper WHERE ? = ?'
+QUERY_GET_ATTRIBUTE_BY_ID = 'SELECT ? FROM paper WHERE paper_id = ?'
+QUERY_GET_CITATIONS_BY_ID = 'SELECT reference_paper_id FROM citation WHERE paper_id = ?'
+QUERY_INSERT_PAPER = 'INSERT INTO paper (paper_id) VALUES (?)'
+QUERY_INSERT_CITATION = 'INSERT INTO citation (paper_id, reference_paper_id) VALUES (?, ?)'
+QUERY_UPDATE_ATTRIBUTE = 'UPDATE paper SET ? = ? WHERE paper_id = ?'
+
 def create_app(test_config=None):
 	if os.path.abspath(os.curdir).find(" ") != -1:
 		raise Exception("Path name contains spaces. Please move the installation folder to one without spaces in the path name.")
@@ -45,18 +54,32 @@ def create_app(test_config=None):
 	app.register_blueprint(ref_info.bp)
 	app.register_blueprint(lookup_paper.bp)
 
+	# TODO if (database doesn't exist yet):
+	init_db(app)
+
+	@app.route('/addpaper', methods = ['POST', 'GET'])
+	def addpaper():
+		if request.method == 'POST':
+			try:
+				paperId = request.form['paperId']
+
+				with sqlite3.connect(DATABASE) as con:
+					cur = con.cursor()
+					cur.execute(QUERY_INSERT_PAPER, (paperId))
+					con.commit()
+			except:
+				con.rollback()
+			finally:
+				con.close()
+
 	return app
 
-#################################################################
-
-DATABASE = 'citation_graph.db'
-QUERY_GET_PAPER_BY_ID = 'SELECT * FROM paper WHERE paper_id = ?'
-QUERY_GET_PAPERS_BY_ATTRIBUTE = 'SELECT * FROM paper WHERE ? = ?'
-QUERY_GET_ATTRIBUTE_BY_ID = 'SELECT ? FROM paper WHERE paper_id = ?'
-QUERY_GET_CITATIONS_BY_ID = 'SELECT reference_paper_id FROM citation WHERE paper_id = ?'
-QUERY_INSERT_PAPER = 'INSERT INTO paper (paper_id) VALUES (?)'
-QUERY_INSERT_CITATION = 'INSERT INTO citation (paper_id, reference_paper_id) VALUES (?, ?)'
-QUERY_UPDATE_ATTRIBUTE = 'UPDATE paper SET ? = ? WHERE paper_id = ?'
+def init_db(app):
+	with app.app_context():
+		db = get_db()
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
 
 def get_db():
 	db = getattr(g, '_database', None)
@@ -64,24 +87,17 @@ def get_db():
 		db = g._database = sqlite3.connect(DATABASE)
 	return db
 
-def init_db():
-	with app.app_context():
-		db = get_db()
-		with app.open_resource('schema.sql', mode='r') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
+def query_db(query, args=(), one=False):
+	cur = get_db().execute(query, args)
+	rv = cur.fetchall()
+	cur.close()
+	return (rv[0] if rv else None) if one else rv
 
 @app.teardown_appcontext
 def close_connection(exception):
 	db = getattr(g, '_database', None)
 	if db is not None:
 		db.close()
-
-def query_db(query, args=(), one=False):
-	cur = get_db().execute(query, args)
-	rv = cur.fetchall()
-	cur.close()
-	return (rv[0] if rv else None) if one else rv
 
 # TODO delete later, here for reference and testing
 def query_db_example_usage():
